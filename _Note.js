03a8716840e16ba9dc249d3188fcdc0dfa17e920
summetreechina <1095909657@qq.com>
2017-11-27 21:14:22 +0800

    view
    最烦界面

--- _Note.js
+++ _Note.js
@@ -11,20 +11,20 @@
 所有子元素自动成为容器成员，称为 Flex 项目（flex item），简称"项目"。
 
 6个属性设置在容器上。
-	flex-direction
-	flex-wrap
-	flex-flow
-	justify-content
-	align-items
-	align-content
+	flex-direction 内部项目排列方向
+	flex-wrap			内部项目是否换行
+	flex-flow			以上两项综合
+	justify-content		内部项目水平对齐方式
+	align-items				内部项目竖向对齐方式
+	align-content			内部内容竖向对齐方式
 
 6个属性设置在项目上
-	order
-	flex-grow
-	flex-shrink
-	flex-basis
+	order					项目本身的排序
+	flex-grow			项目自身的放大
+	flex-shrink		项目自身的缩小
+	flex-basis		项目自身在父元素主轴还有剩余空间时，定义自身所占的空间。
 	flex
-	align-self
+	align-self		项目自身采取不一样的对齐方式
 
 
 
@@ -89,7 +89,43 @@
 					// px 单位没有强制要求
 				}
 		11 嵌套规则
-		
+			正是我想要的，有组织管理的作用，具体用法太复杂，详见'https://www.imooc.com/video/4834'
+		12 @arguments变量
+			原来
+				.border_arg(@w:30px, @c:red, @x:solid){
+					border: @w:30px, @c:red, @x:solid;
+				}
+			简写
+				.border_arg(@w:30px, @c:red, @x:solid){
+					border: @arguments;
+					// @arguments 是less的保留词
+				}
+			应用
+				.test_arg{
+					.border_arg();
+				}
+			拓展应用
+				.test_arg{
+					.border_arg(40);
+				}
+			总结
+				表面上有些复杂，其实都是嵌套的延伸应用
+		13 避免编译、!important以及总结
+			有时候我们需要输出一些不正确的CSS语法或者使用一些LESS不认识的专有语法。
+			要输出这样的值我们可以在字符串前加上一个 ~
+			例如：
+			width:~'calc(100%-35)';
+			height:~'calc(300px-20px)';
+			编译后(.css文件):
+			width: calc(100%-35);
+			height: calc(300px-20px);
+			!important关键字
+			会为所有混合所带来的样式,添加上!important
+		14 导入另外一个less文件
+			@import "不带扩展名的文件名";
+			@import "style.css";  // 原样导入
+			@import (less) "style.css";  // 导入并转化成less文件
+
 
 
 
